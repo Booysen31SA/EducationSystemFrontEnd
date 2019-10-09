1fc49b2cf20bdd8c4bfc6925a72eb4fbca99ce82
Matthew Booysen <MatthewBoo@awards.co.za>
2019-10-09 10:46:15 +0200

    Retirement Requests

---
+++ EducationSystemFrontEnd/Requests/Retirement/RetirementObj.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EducationSystemFrontEnd.Requests.Retirement
+{
+
+    public class RootObject
+    {
+        public RetirementObj retirement { get; set; }
+        public StatusObj Status { get; set; }
+
+    }
+    public class RetirementObj
+    {
+        public String persal_Num { get; set; }
+        public String iD { get; set; }
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public double payout { get; set; }
+    }
+    public class StatusObj
+    {
+        public String persal_Number { get; set; }
+        public String request { get; set; }
+    }
+}
